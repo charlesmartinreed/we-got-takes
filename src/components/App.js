a3df2b1a15a3b23cc15ec5f9dd299fe13970edd1
Charles Martin Reed <charlesmartinreed@icloud.com>
2023-06-29 18:16:23 -0500

    Init commit.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import './../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl font-bold underline'>Hello from Tailwind and Create React App</h1>
+    </div>
+  );
+}
+
+export default App;
